d4cf49db8c363e650ff0c7f368ace62f0822aff3
evan <evanz1215@gmail.com>
2024-06-22 17:09:06 +0800

    project setting

---
+++ src/constants/layout.ts
@@ -0,0 +1,19 @@
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+import { Platform } from "react-native";
+import { colors } from "./tokens";
+
+export const StackScreenWithSearchBar: NativeStackNavigationOptions = {
+  headerLargeTitle: true,
+  headerLargeStyle: {
+    backgroundColor: colors.background,
+  },
+  headerLargeTitleStyle: {
+    color: colors.text,
+  },
+  headerTintColor: colors.text,
+  headerBlurEffect: "prominent",
+  headerShadowVisible: false,
+  ...(Platform.OS === "ios" && {
+    headerTransparent: true,
+  }),
+};
